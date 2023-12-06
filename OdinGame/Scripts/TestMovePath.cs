1bd938af28dbad6791b84058a8c3cf38cdd7e531
or <1584948787@qq.com>
2023-12-06 23:21:23 +0800

    update

--- OdinGame/Scripts/TestMovePath.cs
+++ OdinGame/Scripts/TestMovePath.cs
@@ -5,14 +5,24 @@ using UnityEngine.AI;
 
 public class TestMovePath : MonoBehaviour
 {
+    private NavMeshAgent _agent;
+
     // Start is called before the first frame update
     void Start()
     {
+        _agent = transform.GetComponent<NavMeshAgent>();
+        _agent.updateRotation = false;
+        _agent.updateUpAxis = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (_agent is null)
+        {
+            return;
+        }
+
         //当按下鼠标左键发射射线，检测是否击中background层级的物体，如果是，或者玩家身上的导航组件，并设置导航目标位置
         if (Input.GetMouseButtonDown(0))
         {
@@ -20,7 +30,7 @@ public class TestMovePath : MonoBehaviour
             RaycastHit hitInfo;
             if (Physics.Raycast(ray, out hitInfo, 1000, 1 << LayerMask.NameToLayer("background")))
             {
-                transform.GetComponent<NavMeshAgent>().SetDestination(hitInfo.point);
+                _agent.SetDestination(hitInfo.point);
             }
         }
     }
